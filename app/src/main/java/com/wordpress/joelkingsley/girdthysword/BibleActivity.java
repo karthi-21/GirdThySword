77e3641dbeb4fcf92719455288fedcc0b1d8a6bb
Joel Kingsley <joelkingsley.r@gmail.com>
2017-10-14 21:28:07 +0530

    Added BibleActivity and RewardsActivity.

--- app/src/main/java/com/wordpress/joelkingsley/girdthysword/BibleActivity.java
+++ app/src/main/java/com/wordpress/joelkingsley/girdthysword/BibleActivity.java
@@ -13,21 +13,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.TabHost;
 
-public class HomeActivity extends AppCompatActivity
+public class BibleActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    TabHost tabHost;
-    ListView today;
-    ListView overdue;
-    ListView all;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_bible);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,68 +41,6 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        tabHost = (TabHost) findViewById(R.id.tabhost); // initiate TabHost
-        today = (ListView) findViewById(R.id.today_list);
-        overdue = (ListView) findViewById(R.id.overdue_list);
-        all = (ListView) findViewById(R.id.all_list);
-
-        setupTabHost();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        setupLists();
-    }
-
-    private void setupLists() {
-    }
-
-    private void setupTabHost() {
-
-        TabHost.TabSpec spec; // Reusable TabSpec for each tab
-        tabHost.setup();
-
-        spec = tabHost.newTabSpec("Today");
-        spec.setIndicator("TODAY");
-        spec.setContent(R.id.tab1);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("Overdue");
-        spec.setIndicator("OVERDUE");
-        spec.setContent(R.id.tab2);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("Tomorrow");
-        spec.setIndicator("TOMORROW");
-        spec.setContent(R.id.tab3);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("All");
-        spec.setIndicator("ALL");
-        spec.setContent(R.id.tab4);
-        tabHost.addTab(spec);
-
-        //set tab which one you want to open first time 0 or 1 or 2
-        tabHost.setCurrentTab(0);
-        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                switch(tabId){
-                    case "Today":
-                        break;
-                    case "Overdue":
-                        break;
-                    case "Tomorrow":
-                        break;
-                    case "All":
-                        break;
-                    default:
-                }
-            }
-        });
     }
 
     @Override
@@ -125,7 +56,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.bible, menu);
         return true;
     }
 
@@ -151,10 +82,10 @@ public class HomeActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_home) {
-            // Handle the camera action
-        } else if (id == R.id.nav_bible) {
-            Intent intent = new Intent(HomeActivity.this,BibleActivity.class);
+            Intent intent = new Intent(BibleActivity.this,HomeActivity.class);
             startActivity(intent);
+        } else if (id == R.id.nav_bible) {
+
         } else if (id == R.id.nav_rewards) {
 
         } else if (id == R.id.nav_statistics) {
--- app/src/main/java/com/wordpress/joelkingsley/girdthysword/RewardsActivity.java
+++ app/src/main/java/com/wordpress/joelkingsley/girdthysword/RewardsActivity.java
@@ -1,6 +1,5 @@
 package com.wordpress.joelkingsley.girdthysword;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,21 +12,14 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.ListView;
-import android.widget.TabHost;
 
-public class HomeActivity extends AppCompatActivity
+public class RewardsActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    TabHost tabHost;
-    ListView today;
-    ListView overdue;
-    ListView all;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_rewards);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -48,68 +40,6 @@ public class HomeActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        tabHost = (TabHost) findViewById(R.id.tabhost); // initiate TabHost
-        today = (ListView) findViewById(R.id.today_list);
-        overdue = (ListView) findViewById(R.id.overdue_list);
-        all = (ListView) findViewById(R.id.all_list);
-
-        setupTabHost();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-        setupLists();
-    }
-
-    private void setupLists() {
-    }
-
-    private void setupTabHost() {
-
-        TabHost.TabSpec spec; // Reusable TabSpec for each tab
-        tabHost.setup();
-
-        spec = tabHost.newTabSpec("Today");
-        spec.setIndicator("TODAY");
-        spec.setContent(R.id.tab1);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("Overdue");
-        spec.setIndicator("OVERDUE");
-        spec.setContent(R.id.tab2);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("Tomorrow");
-        spec.setIndicator("TOMORROW");
-        spec.setContent(R.id.tab3);
-        tabHost.addTab(spec);
-
-        spec = tabHost.newTabSpec("All");
-        spec.setIndicator("ALL");
-        spec.setContent(R.id.tab4);
-        tabHost.addTab(spec);
-
-        //set tab which one you want to open first time 0 or 1 or 2
-        tabHost.setCurrentTab(0);
-        tabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
-            @Override
-            public void onTabChanged(String tabId) {
-                switch(tabId){
-                    case "Today":
-                        break;
-                    case "Overdue":
-                        break;
-                    case "Tomorrow":
-                        break;
-                    case "All":
-                        break;
-                    default:
-                }
-            }
-        });
     }
 
     @Override
@@ -125,7 +55,7 @@ public class HomeActivity extends AppCompatActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.home, menu);
+        getMenuInflater().inflate(R.menu.rewards, menu);
         return true;
     }
 
@@ -150,22 +80,17 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_home) {
+        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_bible) {
-            Intent intent = new Intent(HomeActivity.this,BibleActivity.class);
-            startActivity(intent);
-        } else if (id == R.id.nav_rewards) {
+        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_statistics) {
+        } else if (id == R.id.nav_slideshow) {
 
-        } else if (id == R.id.nav_profile) {
+        } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_help) {
+        } else if (id == R.id.nav_share) {
 
-        } else if (id == R.id.nav_settings) {
-
-        } else if (id == R.id.nav_about) {
+        } else if (id == R.id.nav_send) {
 
         }
 
